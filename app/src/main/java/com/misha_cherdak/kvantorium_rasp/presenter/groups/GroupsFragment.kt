e354cd30cf27d95803415acdb271b09891d01ec1
Alex <deadigle@gmail.com>
2021-06-07 17:34:16 +0700

    some fix

--- app/src/main/java/com/misha_cherdak/kvantorium_rasp/presenter/groups/GroupsFragment.kt
+++ app/src/main/java/com/misha_cherdak/kvantorium_rasp/presenter/groups/GroupsFragment.kt
@@ -1,21 +1,29 @@
 package com.misha_cherdak.kvantorium_rasp.presenter.groups
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.misha_cherdak.kvantorium_rasp.R
+import androidx.fragment.app.Fragment
+import com.misha_cherdak.kvantorium_rasp.databinding.FragmentStudGroupBinding
 
 class GroupsFragment : Fragment() {
 
+    private lateinit var _binding: FragmentStudGroupBinding
+    private val binding get() = _binding
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_stud_group_, container, false)
+    ): View {
+        _binding = FragmentStudGroupBinding.inflate(inflater, container, false)
+        return _binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.rvGroups
     }
 
 }
